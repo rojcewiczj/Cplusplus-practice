55c7f84724e2c90aab2d99bac8991daa79751bf5
John Rojcewicz <rojcewiczj@gmail.com>
2020-10-19 15:43:10 -0500

    practice on c++ class inheritance

--- 10_19.cpp
+++ 10_19.cpp
@@ -4,12 +4,28 @@
 #include <algorithm> // for std::find
 #include <iterator> // for std::begin, std::end
 #include <string>
+#include "Account.cpp"
+#include "Savings_Account.h"
 using namespace std;
 
 
 int main()
 {  
-   
+// Use Account class
+cout << "\n=== Account ====================================" << endl;
+Account acc {};
+acc.deposit(2000.0);
+acc.withdraw(500.0);
+
+cout << endl;
+
+Account *p_acc {nullptr};
+p_acc = new Account();
+p_acc -> deposit(1000.0);
+p_acc -> withdraw(500.0);
+delete p_acc;
+
+
 
 
 
---
+++ Account.h
@@ -0,0 +1,15 @@
+#ifndef _ACCOUNT_H_
+#define _ACCOUNT_H_
+
+class Account
+
+{
+    public:
+        void deposit(double amount);
+        void withdraw(double amount);
+        Account();
+        ~Account();
+};
+
+#endif // _ACCOUNT_H_
+
